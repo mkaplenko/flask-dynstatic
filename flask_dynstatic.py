90633f6aa401b40c6d94e623bac4268f752db430
mkaplenko <mkaplenko@brandproduction.ru>
2014-04-10 12:55:57 +0400

    Add to static_html decorator code

--- flask_dynstatic.py
+++ flask_dynstatic.py
@@ -1,13 +1,24 @@
-__author__ = 'mkaplenko'
+from functools import wraps
+import os
 
+static_root = os.path.join(os.path.dirname(__file__), 'static')
 
 views = []
 
 
-def to_static_html(func):
-    def wrapper(*args, **kwargs):
+def to_static_html(path):
+    def decorator(func):
         if func not in views:
-            views.append(func)
-            print views
-        return func(*args, **kwargs)
-    return wrapper
+                views.append((path, func))
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def construct_html():
+    for view in views:
+        with open(os.path.join(static_root, view[0]), 'w') as html_file:
+            html_file.write(view[1]())
